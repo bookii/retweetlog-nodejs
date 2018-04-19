2e09b452bfa10f1354be86214db20d75c1ae27e0
mizuff <flipflop.develop@gmail.com>
2018-04-19 12:42:44 +0900

    add function showing error message

--- routes/client.js
+++ routes/client.js
@@ -1,6 +1,17 @@
 const Twitter = require('twitter');
 require('dotenv').config();
 
+const showUser = (screen_name) => {
+    const params = {screen_name: screen_name};
+    client.get('users/show', params, (error, profile, response) => {
+        if(!error) {
+            console.log(profile);
+        } else {
+            console.log(error);
+        }
+    });
+}
+
 const client = new Twitter({
     consumer_key: process.env.CONSUMER_KEY,
     consumer_secret: process.env.CONSUMER_SECRET,
@@ -9,16 +20,20 @@ const client = new Twitter({
 });
 
 exports.index = (req, res) => {
-    res.render('index');
+    res.render('index', { items: [] });
 };
 
 exports.getRetweetedTweets = (req, res, next) => {
     if (req.body.screen_name) {
+        // showUser(req.body.screen_name);
         const params = {screen_name: req.body.screen_name};
         client.get('statuses/user_timeline', params, (error, tweets, response) => {
-            if(!error) {
+            if (!error) {
                 const retweetedTweets = tweets.filter(tweet => tweet['retweeted_status']);
-                res.send(retweetedTweets);
+                res.render('index', { items: retweetedTweets });
+            } else {
+                const errorMessage = [{ text: error.message || error[0]['message'] }];
+                res.render('index', { items: errorMessage });
             }
         });
     }
