b9289162e4993b7d684f3fcc5d5d51e7d55c92a5
mizuff <flipflop.develop@gmail.com>
2018-04-19 10:11:27 +0900

    Add logger

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const Twitter = require('twitter');
 const client = require('./routes/tweets');
-
+const logger = require('morgan');
 const PORT = process.env.PORT || 5000
+
+app.use(logger('dev'));
 
 app.get('/', (req, res) => res.send('Hello World!'));
 app.get('/home', client.getUserTimeline);
