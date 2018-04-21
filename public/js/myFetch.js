c825e7b3378fd5d3e62c1bfd2aeeeb278f39d6d9
mizuff <flipflop.develop@gmail.com>
2018-04-22 04:19:03 +0900

    Create readMore button

--- public/js/myFetch.js
+++ public/js/myFetch.js
@@ -5,14 +5,31 @@ const getScript = (src) => {
     document.getElementsByTagName('body')[0].appendChild(s);
 };
 
+const myCreateElement = (elementType, attributes) => {
+    let element = document.createElement(elementType)
+    for (key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+    return element;
+}
+
+const createReadMore = (screenName, maxId) => {
+    let f = myCreateElement('form', {method:'post', id: 'readMore'});
+    f.appendChild(myCreateElement('input', {type: 'hidden', name: 'screenName', value: screenName}))
+    f.appendChild(myCreateElement('input', {type: 'hidden', name: 'maxId', value: maxId}));
+    f.appendChild(myCreateElement('input', {type: 'submit', value: 'Read More'}));
+    return f;
+};
+
 const loadRetweets = (form) => {
+    const screenName = form.elements['screenName'].value;
     fetch('/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
           },    
         body: JSON.stringify({
-            screen_name: form.elements['screenName'].value
+            screen_name: screenName
         })
     }).then((response) => {
         return response.json();
@@ -25,6 +42,10 @@ const loadRetweets = (form) => {
         });
     }).then(() => {
         getScript('https://platform.twitter.com/widgets.js');
+        if (!document.getElementById('readMore')) {
+            console.log(document.getElementsByTagName('body')[0]);
+            document.getElementsByTagName('body')[0].appendChild(createReadMore(screenName, null));
+        }
         console.log('Finished!');
     });
 };
