99b50e79732e2adf014f5e2935b437010e066281
mizuff <flipflop.develop@gmail.com>
2018-04-27 08:28:24 +0900

    Append onscroll  function to jumpToTop button

--- public/js/jumpToTop.js
+++ public/js/jumpToTop.js
@@ -1,4 +1,7 @@
-// window.onscroll = () => {
-//     console.log('scrolled');
-//     document.getElementById('jumpToTop').setAttribute('disabled', '');
-// }
+window.onscroll = () => {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("jumpToTop").style.display = "block";
+    } else {
+        document.getElementById("jumpToTop").style.display = "none";
+    }
+}
