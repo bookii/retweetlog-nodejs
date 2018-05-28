3791d3a8875463704c4dad8cca14dcb1625c0ea9
mizuff <flipflop.develop@gmail.com>
2018-05-28 23:42:18 +0900

    Fix createReadMore

--- public/js/myFetch.js
+++ public/js/myFetch.js
@@ -23,12 +23,13 @@ const myCreateElement = (elementType, attributes) => {
     return element;
 }
 
-const createReadMore = (screenName, maxId, csrfToken) => {
+const createReadMore = (screenName, maxId, csrfToken, includeSelf) => {
     let f = myCreateElement('form', {method:'post', id: 'readMore'});
     f.appendChild(myCreateElement('input', {type: 'hidden', name: 'screenName', value: screenName}))
     f.appendChild(myCreateElement('input', {type: 'hidden', name: 'maxId', value: maxId}));
     f.appendChild(myCreateElement('input', {type: 'hidden', name: 'untilDate', value: null}));
     f.appendChild(myCreateElement('input', {type: 'hidden', name: 'reset', value: false}));
+    f.appendChild(myCreateElement('input', {type: 'hidden', name: 'includeSelf', value: includeSelf}));
     f.appendChild(myCreateElement('input', {type: 'hidden', name: '_csrf', value: csrfToken}));
     const readMoreButton = myCreateElement('button', {class: "button is-info", type: 'submit'});
     readMoreButton.appendChild(document.createTextNode('続きを読む'));
@@ -87,7 +88,7 @@ const loadRetweets = (form) => {
             readMoreNode.parentNode.removeChild(readMoreNode);
         }
         if (maxId) {  // add button
-            form = createReadMore(screenName, maxId, csrfToken);
+            form = createReadMore(screenName, maxId, csrfToken, includeSelf);
             document.getElementsByClassName('contents')[0].appendChild(form);
             document.getElementById('readMore').addEventListener("submit", (event) => {
                 event.preventDefault();
